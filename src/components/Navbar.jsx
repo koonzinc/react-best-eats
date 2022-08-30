a5686c4085e9da89219471314e6c8dde0ce6000f
koonzinc <koonz.eth@gmail.com>
2022-08-30 13:52:37 -0400

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {AiOutlineMenu} from 'react-icons/ai';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='max-w-[1640px] mx-auto'>
+            <div>
+                <div>
+                    <AiOutlineMenu size={30} />
+                </div>
+                <h1>
+                    Best <span>Eats</span>
+                </h1>
+                <div>
+                    <p>Delivery</p>
+                    <p>Pickup</p>
+                </div>
+            </div>
+        </div>
     )
 }
 

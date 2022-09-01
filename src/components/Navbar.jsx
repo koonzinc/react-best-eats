66d94dd1efd30ae5fd7c333ebeb808b09b34dda2
koonzinc <koonz.eth@gmail.com>
2022-09-01 19:19:44 -0400

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
                 <nav>
                     <ul className='flex flex-col p-4 text-gray-800'>
                         <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' />Orders</li>
-                        <li className='text-xl py-4 flex'><MdFavorite size={25} className='mr-4' />Favorites</li>
+                        <li className='text-xl py-4 flex'><MdFavorite size={25} className='mr-4' />Favorite</li>
                         <li className='text-xl py-4 flex'><FaWallet size={25} className='mr-4' />Wallet</li>
                         <li className='text-xl py-4 flex'><MdHelp size={25} className='mr-4' />Help</li>
                         <li className='text-xl py-4 flex'><AiFillTag size={25} className='mr-4' />Promotions</li>
